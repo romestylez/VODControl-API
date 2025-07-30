9c4210576540f53e1696a60fddf17de4519e2f17
romestylez <romestylez@users.noreply.github.com>
2025-07-30 20:23:21 +0200

    Update version.php

--- version.php
+++ version.php
@@ -3,4 +3,4 @@ $config = require __DIR__ . '/vod-config.php';
 
 header('Content-Type: text/plain');
 
-echo "📺 24x7 VOD Server - Version 1.0.1 beta 🖥️ created by romestylez & sm4ck_82";
+echo "📺 24x7 VOD Server - Version 1.0.3 beta 🖥️ created by romestylez & sm4ck_82 - https://github.com/romestylez/VODControl-API";
